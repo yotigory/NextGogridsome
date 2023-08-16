ea11d6c50a5260af99917c0496a421426ada4b3c
Yoshiko Sarakai <yoshiko@digitalcube.jp>
2023-08-16 12:28:26 +0900

    {アナリティクス呼び出し}

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import SEO from "../next-seo.config";
 import { DefaultSeo } from "next-seo";
+import ScriptGa from "../ScriptGa";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <DefaultSeo {...SEO} />
+			<DefaultSeo {...SEO} />
+			<ScriptGa />
       <Component className="font-" {...pageProps} />
     </>
   );
